3fc8e1ed4c8d365cfdde0639a14ea40b96224c83
jqim <james.maclean@jqim.co.uk>
2017-06-29 21:05:42 +0100

    defning models

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+var Song = Backbone.Model.extend({
+    initialize: function(){
+        console.log("A new song has been created.");
+    },
+    defaults:{
+        genre: "Jazz"
+    }
+});
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+var song = new Song({
+        artist: "Miles Davis",
+        title: "blue river"
+});
